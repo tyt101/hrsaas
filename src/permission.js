f0e2241a420d1183df8cbd66dc11b17deda69dba
apple <apple@qq.com>
2022-03-10 15:12:54 +0800

    权限

---
+++ src/permission.js
@@ -0,0 +1,25 @@
+import router from "@/router";
+import store from '@/store'
+import nprogress from "nprogress";
+import 'nprogress/nprogress.css'
+const whiteList = ['/login', '/404']
+router.beforeEach((to, from, next) => {
+    nprogress.start()
+    if (store.getters.token) {
+        if (to.path === '/login') {
+            next('/')
+        } else {
+            next()
+        }
+    } else {
+        if (whiteList.indexOf(to.path)) {
+            next()
+        } else {
+            next('/login')
+        }
+    }
+    nprogress.done()
+})
+router.afterEach(() => {
+    nprogress.done()
+})
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'vue_admin_template_token'
-
+const TokenKey = 'hrsaas-ihrm-token'
+    // const timeKey = 'hrsaas-timestamp-key'
 export function getToken() {
     return Cookies.get(TokenKey)
 }
@@ -13,3 +13,11 @@ export function setToken(token) {
 export function removeToken() {
     return Cookies.remove(TokenKey)
 }
+
+// export function getTimeStamp() {
+//     return Cookies.get(timeKey)
+// }
+
+// export function setTimeStamp() {
+//     return Cookies.set(timeKey, Date.now())
+// }
