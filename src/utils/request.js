2c1851c41d2da5029f9bb9f97c361e9335f72c99
apple <apple@qq.com>
2022-03-10 10:31:56 +0800

    设置不同环境中不同的请求地址和基础地址

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const service = axios.create({})
+const service = axios.create({
+    baseURL: process.env.VUE_APP_BASE_API,
+    timeout: 5000
+})
 service.interceptors.request({})
 service.interceptors.response({})
 export default service
