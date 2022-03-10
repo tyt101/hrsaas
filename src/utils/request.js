189d86a7e2f45d3e192a04b16dc1a452af8d9a0c
apple <apple@qq.com>
2022-03-10 10:53:44 +0800

    处理拦截器和结构

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,20 @@
 import axios from 'axios'
-
+import { Message } from 'element-ui'
 const service = axios.create({
     baseURL: process.env.VUE_APP_BASE_API,
     timeout: 5000
 })
-service.interceptors.request({})
-service.interceptors.response({})
+service.interceptors.request.use()
+service.interceptors.response.use(response => {
+    const { succuss, message, data } = response.data
+    if (succuss) {
+        return data
+    } else {
+        Message.error(message)
+        return Promise.reject(new Error(message))
+    }
+}, error => {
+    Message.error(error.Message)
+    return Promise.reject(error)
+})
 export default service
