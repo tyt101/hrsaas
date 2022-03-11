76ab1dbb809e99b79b57f9135a8f1295156db156
apple <apple@qq.com>
2022-03-11 20:32:10 +0800

    删除

---
+++ src/views/departments/components/tree-tool.vue
@@ -0,0 +1,60 @@
+<template>
+<el-row type="flex" justify="space-between" align="middle" style="height: 40px;width:100%">
+    <el-col>
+    <span>{{treeNode.name}}</span>
+    </el-col>
+    <el-col :span="4">
+    <el-row type="flex" justify="end">
+        <el-col>{{treeNode.manager}}</el-col>
+        <el-col>
+        <el-dropdown @command="operateDepts">
+            <span>操作<i class="el-icon-arrow-down"></i></span>
+            <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+            <el-dropdown-item v-if="!isRoot" command="edit">编辑部门</el-dropdown-item>
+            <el-dropdown-item v-if="!isRoot" command="del">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+        </el-dropdown>
+        </el-col>
+    </el-row>
+    </el-col>
+</el-row>
+</template>
+
+<script>
+import {delDepartments} from '@/api/departments'
+export default {
+    props:{
+        treeNode:{
+            require:true,
+            type:Object
+        },
+        isRoot:{
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        operateDepts(type){
+            if(type==='add'){
+
+            }else if(type==='edit'){
+
+            }else{
+                this.$confirm('你确定要删除吗').then(()=>{
+                    return delDepartments(this.treeNode.id)
+                }).then(()=>{
+                    this.$emit('deldepts')
+                    this.$message.success('删除成功')
+                }).catch(err=>{
+                    console.log(err)
+                })
+            }
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+        <treetool :tree-node="company" :isRoot="true"/>
+        <el-tree :data="departs">
+          <treetool slot-scope="{ data }" :tree-node="data" @deldepts="getDepartments" />
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import treetool from './components/tree-tool.vue'
+import {getDepartments} from '@/api/departments'
+import {tranListToTreeData} from '@/utils'
 export default {
-
+  data() {
+    return {
+      company: {},
+      departs: []
+    }
+  },
+  components:{
+    treetool
+  },
+  created(){
+    this.getDepartments()
+  },
+  methods: {
+    async getDepartments(){
+      const result = await getDepartments()
+      this.company = {name:result.companyName,manager:'负责人'}
+      console.log(result)
+      this.departs=tranListToTreeData(result.depts,'')
+      console.log(this.departs)
+    }
+  },
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card{
+  padding: 30px 140px;
+  font-size:14px;
+}
 </style>
